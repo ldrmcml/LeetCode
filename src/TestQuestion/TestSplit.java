b2f4aa08065e8f2e34edaad4451e0eac6b9105e7
ldrmcml <ldrmcml@qq.com>
2014-11-27 01:42:07 +0800

    merge

--- src/TestQuestion/TestSplit.java
+++ src/TestQuestion/TestSplit.java
@@ -5,14 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TestSplit {
-<<<<<<< HEAD
 	private static String data[][]=new String[5][];
 	public static void main(String[] args){
 		/*InputStreamReader isr=new InputStreamReader(System.in);
-=======
-	public static void main(String[] args){
-		InputStreamReader isr=new InputStreamReader(System.in);
->>>>>>> c848aa41463b10b7f0d8d72afcf294b42d705a9a
 		BufferedReader br=new BufferedReader(isr);
 		try {
 			for(String str:br.readLine().split(";"))
@@ -20,13 +15,9 @@ public class TestSplit {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-<<<<<<< HEAD
 		}*/
 		//data[0][1]="aa";
 		System.out.println((-1-2)/2);
-=======
-		}
->>>>>>> c848aa41463b10b7f0d8d72afcf294b42d705a9a
 	}
 
 }
