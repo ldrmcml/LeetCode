53ff7a2e8849f0d84412812afa6787727d6d18a9
ldrmcml <ldrmcml@qq.com>
2014-11-27 01:28:34 +0800

    delete lib and solve conflict

--- src/LeetCode/StringtoInteger.java
+++ src/LeetCode/StringtoInteger.java
@@ -1,5 +1,5 @@
 package LeetCode;
-<<<<<<< HEAD
+
 /**
  * Runtime error.数位累加
  * @author Administrator
@@ -43,37 +43,6 @@ public class StringtoInteger {
 		int num=0;//初始值
 		try {
 			num = atoi("-1123qwe123");
-=======
-
-public class StringtoInteger {
-	public static int atoi(String str) {
-		//null or ""
-		if(str==null||str.equals(""))
-			try {
-				throw new Exception("null or blank string");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		//去掉前后空白字符
-		str=str.trim();
-		for(int i=0;i<str.length();i++)
-			if(!(str.charAt(i)>='0'&&str.charAt(i)<='9'))
-				try {
-					throw new Exception("contain not number");
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		//"0123"
-		int num=Integer.parseInt(str);
-        return num;
-    }
-	public static void main(String[] args){
-		int num=0;//初始值
-		try {
-			num = atoi("");
->>>>>>> c848aa41463b10b7f0d8d72afcf294b42d705a9a
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
