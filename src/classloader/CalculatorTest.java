e17ff291e09926830359fa3903b0fd78d4439797
ldrmcml <ldrmcml@qq.com>
2014-12-17 11:07:47 +0800

    modify CalculatorTest.java

--- src/classloader/CalculatorTest.java
+++ src/classloader/CalculatorTest.java
@@ -5,8 +5,8 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		String url = "http://localhost:8080/ClassloaderTest/classes";
 		NetworkClassLoader ncl = new NetworkClassLoader(url);
-		String basicClassName = "com.example.CalculatorBasic";
-		String advancedClassName = "com.example.CalculatorAdvanced";
+		String basicClassName = "classloader.example.CalculatorBasic";
+		String advancedClassName = "classloader.example.CalculatorAdvanced";
 		try {
 			Class<?> clazz = ncl.loadClass(basicClassName);
 			ICalculator calculator = (ICalculator) clazz.newInstance();
